b93be00bd85b730b0c73bcfdb60935830c20b344
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-04-18 15:28:40 +0100

    Feat: create the upload to cloudinary controller

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 const helmet = require('helmet')
 const xss = require('xss-clean')
 const path = require('path')
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
 
 require('dotenv').config();
 require('express-async-errors')
@@ -13,12 +14,20 @@ const productRouter = require('./routes/productRoute')
 const errorHandler = require('./middleware/errorHandler')
 const app = express()
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
 app.use(cors())
 app.use(helmet())
 app.use(xss())
 
 app.use(express.json());
-app.use(fileUpload())
+app.use(fileUpload({
+    useTempFiles: true
+}))
 
 app.use(express.static(path.resolve('./public')))
 app.use('/api/v1/products', productRouter)
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,32 +1,60 @@
-const Product = require('../model/Product')
-const { StatusCodes } = require('http-status-codes')
-const path = require('path')
-const { badRequestError } = require('../utils/CustomError')
+const Product = require('../model/Product');
+const { StatusCodes } = require('http-status-codes');
+const path = require('path');
+const { badRequestError } = require('../utils/CustomError');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+
+const uploadProductImageLocal = async (req, res) => {
+  if (!req.files) {
+    throw badRequestError('No file uploaded');
+  }
+
+  const { image } = req.files;
+  if (!image.mimetype.startsWith('image')) {
+    throw badRequestError('Please upload an image file');
+  }
+  const maxSize = 1024 * 1024 * 2;
+  if (image.size > maxSize) {
+    throw badRequestError('File size should not be larger than 2MB');
+  }
+  const filePath = path.join(__dirname, `../public/uploads/${image.name}`);
+  await image.mv(filePath);
+  res.status(StatusCodes.OK).json({
+    code: '00',
+    msg: 'Image uploaded successfully',
+    image: {
+      src: `/uploads/${image.name}`,
+    },
+  });
+};
 
 const uploadProductImage = async (req, res) => {
-    if(!req.files) {
-        throw badRequestError('No file uploaded')
+  if (!req.files) {
+    throw badRequestError('No file uploaded');
+  }
+  const { image } = req.files;
+  if (!image.mimetype.startsWith('image')) {
+    throw badRequestError('Please upload an image file');
+  }
+  const maxSize = 1024 * 1024 * 2;
+  if (image.size > maxSize) {
+    throw badRequestError('File size should not be larger than 2MB ');
+  }
+  const result = await cloudinary.uploader.upload(image.tempFilePath, {
+    folder: 'file-upload',
+    use_filename: true,
+  });
+  fs.unlinkSync(image.tempFilePath);
+  res.status(StatusCodes.OK).json({
+    code: '00',
+    msg: 'Image uploaded successfully',
+    image: {
+        src: result.secure_url,
     }
-
-    const { image } = req.files;
-    if(!image.mimetype.startsWith('image')) {
-        throw badRequestError('Please upload an image file')
-    }
-    const maxSize = 1024 * 1024 * 2;
-    if(image.size > maxSize) {
-        throw badRequestError('File size should not be larger than 2MB')
-    }
-    const filePath = path.join(__dirname, `../public/uploads/${image.name}`)
-    await image.mv(filePath)
-    res.status(StatusCodes.OK).json({
-        code: '00',
-        msg: 'Image uploaded successfully',
-        image: {
-            src: `/uploads/${image.name}`
-        }
-    })
-}
+  })
+};
 
 module.exports = {
-    uploadProductImage
-}
+  uploadProductImage,
+};
